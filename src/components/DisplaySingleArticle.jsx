2c25f72eeddbca4d61edf6aa27ae0b4639e5a25a
Blake <blake@futchi.com>
2020-03-31 17:12:12 +0200

    added back button, need to create redux action

--- src/components/DisplaySingleArticle.jsx
+++ src/components/DisplaySingleArticle.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { IonContent, IonGrid } from '@ionic/react';
+import { IonContent, IonGrid, IonButton } from '@ionic/react';
 
 
 const DisplaySingleArticle = props => {
   let articleDetails;
   let article = props.singleArticle;
   articleDetails = (
+    <>
     <IonGrid key={article.id} align="center">
       <h1>{article.title}</h1>
       <h4>{article.lead}</h4>
       <p>{article.content}</p>
     </IonGrid>
+    <IonButton
+          id="back-button"
+          onClick={() => props.dispatch({ type: "BACK_TO_ARTICLE_LIST" })}
+          key={article.id}
+        >
+         Back
+        </IonButton>
+    </>
   );
   return <IonContent id="single-article">{articleDetails}</IonContent>;
 };
